7e5761b97d7391236a560301deefa1418a3b20dc
Student <Student@1912846-C20246.lanecc.edu>
2018-05-21 14:01:50 -0700

    Testing Lab 4

--- CustomerMaintenanceStart/CustomerMaintenanceClasses/Customer.cs
+++ CustomerMaintenanceStart/CustomerMaintenanceClasses/Customer.cs
@@ -8,6 +8,50 @@ namespace CustomerMaintenanceClasses
 {
     public class Customer
     {
+        private string firstName;
+        private string lastName;
+        private string email;
 
+        public Customer()
+        {
+            firstName = "John";
+            lastName = "Doe";
+            email = "john@doe.com";
+        }
+
+        public Customer(string fn, string ln, string e)
+        {
+            firstName = fn;
+            lastName = ln;
+            email = e;
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string GetDisplayText()
+        {
+            return firstName + " " + lastName + ", " + email + ".";
+        }
+
+        public override string ToString()
+        {
+            return firstName + " " + lastName + " " + email;
+        }
     }
 }
--- CustomerMaintenanceStart/CustomerTests/Program.cs
+++ CustomerMaintenanceStart/CustomerTests/Program.cs
@@ -11,7 +11,58 @@ namespace CustomerTests
     {
         static void Main(string[] args)
         {
+            TestCustomerConstructors();
+            TestCustomerGetters();
+            TestCustomerSetters();
+            TestCustomerToString();
 
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        static void TestCustomerConstructors()
+        {
+            Customer c1 = new Customer();
+            Customer c2 = new Customer("Nohm", "Chomskey", "nohm@chomskey.com");
+
+            Console.WriteLine("Testing both constructors");
+            Console.WriteLine("Default constructor.  Expecting default values. " + c1.GetDisplayText());
+            Console.WriteLine("Overloaded constructor.  Expecting Nohm Chomskey, nohmchomskey.com. ");
+            Console.WriteLine("Getting " + c2.GetDisplayText());
+            Console.WriteLine();
+        }
+
+        static void TestCustomerGetters()
+        {
+            Customer c1 = new Customer("Nohm", "Chomskey", "nohm@chomskey.com");
+
+            Console.WriteLine("Testing getters");
+            Console.WriteLine("First name.  Expecting Nohm. " + c1.FirstName);
+            Console.WriteLine("Last name.  Expecting Chomskey. " + c1.LastName);
+            Console.WriteLine("Email.  Expecting nohm@chomskey.com. " + c1.Email);
+            Console.WriteLine();
+        }
+
+        static void TestCustomerSetters()
+        {
+            Customer c1 = new Customer("Nohm", "Chomskey", "nohm@chomskey.com");
+
+            Console.WriteLine("Testing setters");
+            c1.FirstName = "Kyle";
+            c1.LastName = "Normand";
+            c1.Email = "kyle@normand.com";
+            Console.WriteLine("Expecting Kyle, Normand, kyle@normand.com.");
+            Console.WriteLine("Getting " + c1.GetDisplayText());
+            Console.WriteLine();
+        }
+        static void TestCustomerToString()
+        {
+            Customer c1 = new Customer("Nohm", "Chomskey", "nohm@chomskey.com");
+
+            Console.WriteLine("Testing ToString");
+            Console.WriteLine("Expecting Nohm, Chomskey, nohm@chomskey.com. " + c1.GetDisplayText());
+            Console.WriteLine("Getting Nohm, Chomskey, nohm@chomskey.com. " + c1.ToString());
+            Console.WriteLine();
         }
     }
 }
